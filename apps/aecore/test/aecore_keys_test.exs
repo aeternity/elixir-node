1fd9674fd93cfc32cb7d5ce269895da8fb50238b
meivantodorov <meivantodorov@gmail.com>
2017-09-27 16:49:48 +0300

    [GH-#11] Removed some config declarations. More formatting fixes

--- apps/aecore/test/aecore_keys_test.exs
+++ apps/aecore/test/aecore_keys_test.exs
@@ -13,15 +13,15 @@ defmodule AecoreKeysTest do
   end
 
   test "test if a pubkey is loaded" do
-	assert {:ok,_key} = Keys.pubkey()
+    assert {:ok,_key} = Keys.pubkey()
   end
 
   test "sign fake coinbase transaction and verify the signed transaction" do
-	{:ok, from_acc} = Keys.pubkey()
-	coinbase_tx = %{:from_acc => from_acc,
-					:to_acc   => "to account",
-					:value    => 5}
-	assert {:ok, %{data: data,signature: signature}} = Keys.sign(coinbase_tx)
-	assert :true = Keys.verify(data,signature,data.from_acc)
+    {:ok, from_acc} = Keys.pubkey()
+    coinbase_tx = %{:from_acc => from_acc,
+                    :to_acc   => "to account",
+                    :value    => 5}
+    assert {:ok, %{data: data,signature: signature}} = Keys.sign(coinbase_tx)
+    assert :true = Keys.verify(data,signature,data.from_acc)
   end
 end
