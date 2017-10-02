e5824e5861f0c2af225bb5e9885815efaec245a5
d-velev <bamzorlol@gmail.com>
2017-10-02 18:07:13 +0300

    Added validation functions, GenServer not yet implemented

---
+++ apps/aecore/lib/aecore/validation/worker/supervisor.ex
@@ -0,0 +1,14 @@
+defmodule Aecore.Validation.Worker.Supervisor do
+  use Supervisor
+
+  def start_link() do
+    Supervisor.start_link(__MODULE__, :ok)
+  end
+
+  def init(:ok) do
+    children = [
+      worker(Aecore.Validation.Worker, [])
+    ]
+	supervise(children, strategy: :one_for_one)
+  end
+end
