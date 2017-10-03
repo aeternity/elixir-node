c4f17e919cf6b4ca14d457a8951b1aa7cb5dbfbd
Nedelcho Delchev <nedelcho_delchev@abv.bg>
2017-10-03 16:59:52 +0300

    [GH-#5] Fixed tests not working sometimes

--- apps/aecore/test/aecore_chain_test.exs
+++ apps/aecore/test/aecore_chain_test.exs
@@ -19,17 +19,8 @@ defmodule AecoreChainTest do
     assert :ok = Chain.add_block(block)
     assert latest_block = Chain.latest_block()
     assert latest_block.header.height == block.header.height
-  end
-
-  test "latest block" do
-    block = %Block{header: %Header{height: 1, prev_hash: <<1, 24, 45>>, txs_hash: <<12, 123, 12>>, difficulty_target: 0, nonce: 0, timestamp: System.system_time(:milliseconds), version: 1}, txs: []}
-    assert latest_block = Chain.latest_block()
-    assert latest_block.header.height == block.header.height
-  end
-
-  test "all blocks" do
     length = length(Chain.all_blocks())
     assert length == 2
   end
-  
+
 end
