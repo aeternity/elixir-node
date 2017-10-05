3b0da5e580f9e49750ece1ee9ef3ab225f2ad901
Nedelcho Delchev <nedelcho_delchev@abv.bg>
2017-10-05 16:58:48 +0300

    [GH-#8] Refactoring and added test for miner

--- apps/aecore/lib/aecore/chain/worker.ex
+++ apps/aecore/lib/aecore/chain/worker.ex
@@ -5,13 +5,6 @@ defmodule Aecore.Chain.Worker do
 
   alias Aecore.Block.Genesis
   alias Aecore.Structures.Block
-  alias Aecore.Utils.Blockchain.BlockValidation
-  alias Aecore.Utils.Blockchain.Difficulty
-  alias Aecore.Structures.TxData
-  alias Aecore.Structures.SignedTx
-  alias Aecore.Block.Headers
-  alias Aecore.Block.Blocks
-  alias Aecore.Pow.Hashcash
 
   use GenServer
 
@@ -23,20 +16,19 @@ defmodule Aecore.Chain.Worker do
     {:ok, initial_chain}
   end
 
+  @spec latest_block() :: %Block{}
   def latest_block() do
     GenServer.call(__MODULE__, :latest_block)
   end
 
+  @spec all_blocks() :: list()
   def all_blocks() do
     GenServer.call(__MODULE__, :all_blocks)
   end
 
+  @spec add_block(%Block{}) :: :ok
   def add_block(%Block{} = b) do
     GenServer.call(__MODULE__, {:add_block, b})
-  end
-
-  def mine_next_block(txs) do
-    GenServer.call(__MODULE__, {:txs, txs}, :infinity)
   end
 
   def handle_call(:latest_block, _from, chain) do
@@ -49,28 +41,7 @@ defmodule Aecore.Chain.Worker do
   end
 
   def handle_call({:add_block, %Block{} = b}, _from, chain) do
-    #TODO validations
     {:reply, :ok, [b | chain]}
-  end
-
-  def handle_call({:txs, txs}, _from, chain) do
-    difficulty = Difficulty.calculate_next_difficulty(chain)
-    valid_txs = BlockValidation.filter_invalid_transactions(txs)
-    latest_block = if(length(chain) == 1) do
-      [latest_block | _] = chain
-      latest_block
-    else
-      [latest_block, previous_block | _] = chain
-      BlockValidation.validate_block!(latest_block, previous_block)
-      latest_block
-    end
-    root_hash = BlockValidation.calculate_root_hash(valid_txs)
-    latest_block_hash = BlockValidation.block_header_hash(latest_block)
-    block_header = Headers.new(latest_block.header.height + 1, latest_block_hash, root_hash, difficulty, 0, 1)
-    {:ok, mined_header} = Hashcash.generate(block_header)
-    {:ok, block} = Blocks.new(mined_header, valid_txs)
-    IO.inspect(block)
-    {:reply, :ok, [block | chain]}
   end
 
 end
--- apps/aecore/lib/aecore/miner/worker.ex
+++ apps/aecore/lib/aecore/miner/worker.ex
@@ -4,6 +4,15 @@ defmodule Aecore.Miner.Worker do
   """
 
   alias Aecore.Chain.Worker, as: Chain
+  alias Aecore.Block.Genesis
+  alias Aecore.Structures.Block
+  alias Aecore.Utils.Blockchain.BlockValidation
+  alias Aecore.Utils.Blockchain.Difficulty
+  alias Aecore.Structures.TxData
+  alias Aecore.Structures.SignedTx
+  alias Aecore.Block.Headers
+  alias Aecore.Block.Blocks
+  alias Aecore.Pow.Hashcash
 
   use GenServer
 
@@ -16,14 +25,32 @@ defmodule Aecore.Miner.Worker do
     {:ok, []}
   end
 
-  def handle_info(:work, state) do
-    state = miner(state)
-    schedule_work()
-    {:noreply, state}
+  @spec mine_next_block(list()) :: :ok
+  def mine_next_block(txs) do
+    chain = Chain.all_blocks()
+    difficulty = Difficulty.calculate_next_difficulty(chain)
+    valid_txs = BlockValidation.filter_invalid_transactions(txs)
+    latest_block = if(length(chain) == 1) do
+      [latest_block | _] = chain
+      latest_block
+    else
+      [latest_block, previous_block | _] = chain
+      BlockValidation.validate_block!(latest_block, previous_block)
+      latest_block
+    end
+    root_hash = BlockValidation.calculate_root_hash(valid_txs)
+    latest_block_hash = BlockValidation.block_header_hash(latest_block)
+    block_header = Headers.new(latest_block.header.height + 1, latest_block_hash, root_hash, difficulty, 0, 1)
+    {:ok, mined_header} = Hashcash.generate(block_header)
+    {:ok, block} = Blocks.new(mined_header, valid_txs)
+    IO.inspect(block)
+    Chain.add_block(block)
   end
 
-  defp miner(state) do
-    Chain.mine_next_block([])
+  def handle_info(:work, state) do
+    mine_next_block([])
+    schedule_work()
+    {:noreply, state}
   end
 
   defp schedule_work do
