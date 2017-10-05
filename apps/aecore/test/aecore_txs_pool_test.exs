b3a1ee7907db4d8feb9128d1fc4218f4c0bc18e6
d-velev <bamzorlol@gmail.com>
2017-10-05 16:57:09 +0300

    [GH-#30] Fixed unit testing for the pool worker module

--- apps/aecore/test/aecore_txs_pool_test.exs
+++ apps/aecore/test/aecore_txs_pool_test.exs
@@ -11,19 +11,11 @@ defmodule AecoreTxsPoolTest do
     []
   end
 
-  test "add transaction to pool" do
+  test "add transaction, remove it and get pool" do
     assert :ok = Pool.add_transaction(
       Aecore.Txs.Tx.create(Aecore.Keys.Worker.pubkey(), 5))
-  end
-
-  test "remove transaction from pool" do
     assert :ok = Pool.remove_transaction(Aecore.Txs.Tx.create(
       Aecore.Keys.Worker.pubkey(), 5))
-  end
-
-  test "get pool" do
-    Pool.add_transaction(
-      Aecore.Txs.Tx.create(Aecore.Keys.Worker.pubkey(), 5))
     tx_pool = Pool.get_pool()
     assert Enum.count(tx_pool) == 1
   end
