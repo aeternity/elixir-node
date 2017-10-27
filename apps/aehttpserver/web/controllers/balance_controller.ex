8cf9c1351badf163fb493d155271495b77bf8d51
d-velev <bamzorlol@gmail.com>
2017-10-27 16:02:44 +0300

    [GH-#89] BalanceController.show refactored

--- apps/aehttpserver/web/controllers/balance_controller.ex
+++ apps/aehttpserver/web/controllers/balance_controller.ex
@@ -4,18 +4,16 @@ defmodule Aehttpserver.BalanceController do
   alias Aecore.Chain.Worker, as: Chain
 
   def show(conn, params) do
-    acc =
       case(Base.decode16(params["account"])) do
         :error ->
           "can't be decoded"
         {:ok, acc} ->
-          acc
+          case(Chain.chain_state[acc]) do
+            nil ->
+              json conn, %{"unknown" => 0}
+            %{balance: balance} ->
+              json conn, %{params["account"] => balance}
+          end
       end
-    case(Chain.chain_state[acc]) do
-      nil ->
-        json conn, %{"unknown" => 0}
-      %{balance: balance} ->
-        json conn, %{params["account"] => balance}
-    end
   end
 end
