5e10c0250445b90681b809ccac8ec6fc06487388
HristianHristov <hristian.hristov@quanterall.com>
2017-11-01 17:41:16 +0200

    [GH-#92] Test refactoring

--- apps/aecore/test/aecore_get_txs_for_address.ex
+++ apps/aecore/test/aecore_get_txs_for_address.ex
@@ -19,14 +19,17 @@ defmodule GetTxsForAddressTest do
   end
 
   @tag timeout: 100000
-  test "test2" do
+  test "get txs for given address test" do
 
     {:ok, address} = Keys.pubkey()
-    address_hex = address |> Base.encode16()
-    address_hex_new = String.slice(address_hex, 1,String.length(address_hex))
-    address_hex_new = address_hex_new  <> "1"
-    address_bin_new = address_hex_new |> Base.decode16!()
-    user_pubkey = {:ok, address_bin_new}
+    address_hex =
+      address
+      |> Base.encode16()
+      |> String.slice(1, String.length(Base.encode16(address)))
+      |> Kernel.<> "1"
+    address_bin = address_hex |> Base.decode16!()
+
+    user_pubkey = {:ok, address_bin}
 
     {:ok, tx1} = Keys.sign_tx(elem(Keys.pubkey(), 1), 5, 1)
     {:ok, tx2} = Keys.sign_tx(elem(user_pubkey, 1), 7, 1)
@@ -36,32 +39,30 @@ defmodule GetTxsForAddressTest do
     assert :ok = Pool.add_transaction(tx2)
     assert :ok = Pool.add_transaction(tx3)
     Miner.resume()
-    :timer.sleep(20000)
+    :timer.sleep(10000)
     Miner.suspend()
 
     assert 2 <= :erlang.length(Chain.all_blocks)
-
-
-    assert [tx2] = split_blocks(Chain.all_blocks, address_bin_new, [])
+    assert [tx2] = split_blocks(Chain.all_blocks, address_bin, [])
   end
 
 
-  defp split_blocks([head | tail], address, acc) do
-    acc1 = check_address_tx(head.txs, address, acc)
-    split_blocks(tail, address, acc1)
+  defp split_blocks([block | blocks], address, txs) do
+    user_txs = check_address_tx(block.txs, address, txs)
+    split_blocks(blocks, address, user_txs)
   end
-  defp split_blocks([], address, acc) do
-    acc
+  defp split_blocks([], address, txs) do
+    txs
   end
 
-  defp check_address_tx([head|tail], address, acc) do
-    if head.data.from_acc == address or head.data.to_acc == address  do
-      acc = [head.data | acc]
+  defp check_address_tx([tx | txs], address, user_txs) do
+    if tx.data.from_acc == address or tx.data.to_acc == address  do
+      user_txs = [tx.data | user_txs]
     end
 
-    check_address_tx(tail, address, acc)
+    check_address_tx(txs, address, user_txs)
   end
-   defp check_address_tx([], address, acc) do
-    acc
+   defp check_address_tx([], address, user_txs) do
+    user_txs
   end
 end
