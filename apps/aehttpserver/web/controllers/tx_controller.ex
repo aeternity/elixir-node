623c7f9aa3c5c32977db6083034b0a44144976cf
HristianHristov <hristian.hristov@quanterall.com>
2017-11-14 18:16:18 +0200

    [GH-#92] Added ?include_proof=true case for including merkle proof

--- apps/aehttpserver/web/controllers/tx_controller.ex
+++ apps/aehttpserver/web/controllers/tx_controller.ex
@@ -4,17 +4,25 @@ defmodule Aehttpserver.TxController do
   alias Aecore.Utils.Serialization, as: Serialization
 
   def show(conn, params) do
+    IO.inspect params
     account_bin =
       params["account"]
       |> Base.decode16!()
     IO.inspect account_bin
     user_txs = Pool.get_txs_for_address(account_bin)
     IO.inspect user_txs
-    json(conn , Enum.map(user_txs, fn(tx) ->
-          %{tx |
-            from_acc: Serialization.hex_binary(tx.from_acc, :serialize),
-            to_acc: Serialization.hex_binary(tx.to_acc, :serialize)
-           } end))
+    case params["include_proof"]  do
+     " true" ->
+        json(conn , Enum.map(user_txs, fn(tx) ->
+              %{tx |
+                from_acc: Serialization.hex_binary(tx.from_acc, :serialize),
+                to_acc: Serialization.hex_binary(tx.to_acc, :serialize)
+               } end))
+      _ ->
+        json(conn , Enum.map(user_txs, fn(tx) ->
+              %{tx |
+                from_acc: Serialization.hex_binary(tx.from_acc, :serialize),
+                to_acc: Serialization.hex_binary(tx.to_acc, :serialize)               } end))
+    end
   end
-
 end
