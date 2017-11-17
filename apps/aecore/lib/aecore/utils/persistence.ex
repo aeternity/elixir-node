924659f970556cc7a521517499cbac1e448dc2b5
meivantodorov <meivantodorov@gmail.com>
2017-11-17 08:07:33 +0200

    [GH-#104] Persistence table path is now evaluated on runtime

--- apps/aecore/lib/aecore/utils/persistence.ex
+++ apps/aecore/lib/aecore/utils/persistence.ex
@@ -3,29 +3,34 @@ defmodule Aecore.Utils.Persistence do
   Store/Restore latest blockchain and chainstate
   """
 
-  @persistence_table Application.get_env(:aecore, :persistence)[:table]
   @blockchain_key :block_chain_state_key
 
+  @spec store_state(state :: tuple()) :: :ok
   def store_state(state) do
-    {:ok, table} = :dets.open_file(@persistence_table , [type: :set])
+    {:ok, table} = :dets.open_file(persistence_table_path() , [type: :set])
     :ok = :dets.insert(table, {@blockchain_key, state})
-    halt_dets(table)
+    :ok = halt_dets(table)
   end
 
   @spec get_block_chain_states() :: {:ok, term()}
   def get_block_chain_states() do
-    {:ok, table} = :dets.open_file(@persistence_table , [type: :set])
+    {:ok, table} = :dets.open_file(persistence_table_path() , [type: :set])
     resp = case :dets.lookup(table, @blockchain_key) do
              [] -> {:ok, :nothing_to_restore}
              restored_data -> {:ok, Keyword.fetch!(restored_data, @blockchain_key)}
            end
-    halt_dets(table)
+    :ok = halt_dets(table)
     resp
   end
 
   defp halt_dets(table) do
-    :dets.close(table)
-    :dets.stop()
+    :ok = :dets.close(table)
+    :stopped = :dets.stop()
+    :ok
+  end
+
+  defp persistence_table_path() do
+    Application.get_env(:aecore, :persistence)[:table]
   end
 
 end
