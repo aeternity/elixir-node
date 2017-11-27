52430f0ccb109a6bf4618a83fd2008442371c763
Philipp Piwo <github@piwo.space>
2017-11-27 18:24:03 +0200

    [GH-#147] get blocks route from_hash with count

--- apps/aehttpserver/web/router.ex
+++ apps/aehttpserver/web/router.ex
@@ -13,6 +13,7 @@ defmodule Aehttpserver.Router do
     post "/new_tx", NewTxController, :new_tx
     get "/peers", PeersController, :info
     post "/new_block", BlockController, :new_block
+    get "/blocks", BlockController, :get_blocks
     resources "/block", BlockController, param: "hash", only: [:show]
     resources "/balance", BalanceController, param: "account", only: [:show]
     resources "/tx_pool", TxPoolController, param: "account", only: [:show]
