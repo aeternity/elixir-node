1b3a665e5192df222604400255c30b2450573182
Artur Puzio <cytadela8@interia.pl>
2017-11-28 23:41:55 +0100

    [GH-114] Added basic structure of aeutil

---
+++ apps/aeutil/mix.exs
@@ -0,0 +1,21 @@
+defmodule Aeutil.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :aeutil,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+end
