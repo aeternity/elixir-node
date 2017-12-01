e2bd191f7bda38ae98ff6f6cae09f07f1477547e
HristianHristov <hristian.hristov@quanterall.com>
2017-12-01 18:26:13 +0200

    [GH-#92] Added functionality for producing a merkle proof and validating it

--- apps/aehttpserver/web/controllers/tx_controller.ex
+++ apps/aehttpserver/web/controllers/tx_controller.ex
@@ -6,48 +6,59 @@ defmodule Aehttpserver.TxController do
   alias Aecore.Utils.Blockchain.BlockValidation
 
   def show(conn, params) do
-   # IO.inspect params["account"]
+    # IO.inspect params["account"]
     account_bin =
       params["account"]
       |> Base.decode16!()
     #IO.inspect account_bin
     user_txs = Pool.get_txs_for_address(account_bin, :no_hash)
-    user_proof = Pool.get_txs_for_address(account_bin, :add_hash)
-    IO.inspect user_proof
-    if length(user_proof) == 0 do
+    user_txs_and_hash = Pool.get_txs_for_address(account_bin, :add_hash)
+    #IO.inspect user_proof
+    merkle_tree = build_tx_tree(user_txs)
+    {size, {key, value, _}} = merkle_tree
+    proof = :gb_merkle_trees.merkle_proof(key, merkle_tree)
+    root_hash =  merkle_tree |> :gb_merkle_trees.root_hash
+    valid = :gb_merkle_trees.verify_merkle_proof(key, value, root_hash, proof)
+    include_proof =
+    for proof <- user_txs_and_hash do
+      Map.put_new(proof, :proof, proof)
+    end
+    #IO.inspect proof
+    #IO.inspect include_proof
+    case params["merkle_proof"]  do
+      "true" ->
+        json(conn, Enum.map(include_proof, fn(tx) ->
+              %{tx |
+                proof: Serialization.hex_binary(tx.proof, :serialize),
+                from_acc: Serialization.hex_binary(tx.from_acc, :serialize),
+                to_acc: Serialization.hex_binary(tx.to_acc, :serialize)
+               } end))
+      _ ->
+        json(conn, Enum.map(user_txs, fn(tx) ->
+              %{tx |
+                from_acc: Serialization.hex_binary(tx.from_acc, :serialize),
+                to_acc: Serialization.hex_binary(tx.to_acc, :serialize)
+               } end))
+    end
+  end
+
+
+  ## TODO : better use pattern matching for len checking : [_h | _] = txs
+  defp build_tx_tree(txs) do
+    if length(txs) == 0 do
       <<0::256>>
     else
       merkle_tree =
-      for transaction <- user_txs do
-        transaction_data_bin = :erlang.term_to_binary(transaction)
+      for transaction <- txs do
+      transaction_data_bin = :erlang.term_to_binary(transaction)
         {:crypto.hash(:sha256, transaction_data_bin), transaction_data_bin}
       end
 
       merkle_tree =
         merkle_tree
-        |> List.foldl(:gb_merkle_trees.empty(), fn node, merkle_tree ->
-        :gb_merkle_trees.enter(elem(node, 0), elem(node, 1), merkle_tree)
+      |> List.foldl(:gb_merkle_trees.empty(), fn(node, merkle_tree_acc) ->
+        :gb_merkle_trees.enter(elem(node, 0), elem(node, 1), merkle_tree_acc)
       end)
-        IO.inspect merkle_tree
-        user_result =
-        for user_result <- user_proof do
-          :gb_merkle_trees.merkle_proof(user_result.txs_hash,
-            merkle_tree)
-          IO.inspect user_result
-        end
-        case params["include_proof"]  do
-          "true" ->
-            json(conn , Enum.map(user_txs, fn(tx) ->
-                  %{tx |
-                    from_acc: Serialization.hex_binary(tx.from_acc, :serialize),
-                    to_acc: Serialization.hex_binary(tx.to_acc, :serialize)
-                   } end))
-            _ ->
-            json(conn , Enum.map(user_proof, fn(tx) ->
-                  %{tx | txs_hash: Serialization.hex_binary(tx.txs_hash, :serialize),
-                    from_acc: Serialization.hex_binary(tx.from_acc, :serialize),
-                    to_acc: Serialization.hex_binary(tx.to_acc, :serialize)               } end))
-        end
     end
   end
 end
