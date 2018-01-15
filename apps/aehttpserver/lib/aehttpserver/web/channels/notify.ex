531d8bac10ded7b5b377a858609100558abdddc1
danielaivanova <danielaivanova_@abv.bg>
2018-01-15 16:02:32 +0200

    [GH-#183] Code refactoring

--- apps/aehttpserver/lib/aehttpserver/web/channels/notify.ex
+++ apps/aehttpserver/lib/aehttpserver/web/channels/notify.ex
@@ -1,23 +1,17 @@
 defmodule Aehttpserver.Web.Notify do
+  alias Aeutil.Serialization
 
-  def broadcast({:new_transaction_in_the_pool_per_account, acc}) do
-  	Aehttpserver.Web.Endpoint.broadcast!("room:notifications","new_tx:" <> acc, %{"body" => "!!!!!!New transaction in the pool!!!!!!"})
+  def broadcast_new_transaction_in_the_pool(from_acc, to_acc, tx) do
+  	Aehttpserver.Web.Endpoint.broadcast!("room:notifications","new_tx:" <> from_acc, %{"body" => Serialization.tx(tx, :serialize)})
+    Aehttpserver.Web.Endpoint.broadcast!("room:notifications","new_tx:" <> to_acc, %{"body" => Serialization.tx(tx, :serialize)})
+    Aehttpserver.Web.Endpoint.broadcast!("room:notifications", "new_transaction_in_the_pool", %{"body" => Serialization.tx(tx, :serialize)})
   end
 
-  def broadcast({:new_transaction_in_the_pool_every}) do
-  	Aehttpserver.Web.Endpoint.broadcast!("room:notifications", "new_transaction_in_the_pool", %{"body" => "!!!!!!New transaction in the pool for everyone!!!!!!"})
+  def broadcast_new_mined_transaction(acc, tx) do
+  	Aehttpserver.Web.Endpoint.broadcast!("room:notifications", "new_mined_tx:" <> acc, %{"body" => Serialization.tx(tx, :serialize)})
   end
 
-  def broadcast({:new_mined_transaction, acc}, tx_data) do
-    data = %{fee: tx_data.fee, 
-             from_acc: Base.encode16(tx_data.from_acc), 
-             to_acc: Base.encode16(tx_data.to_acc), 
-             value: tx_data.value}
-    {:ok, json} = Poison.encode(data)
-  	Aehttpserver.Web.Endpoint.broadcast!("room:notifications", "new_mined_tx:" <> acc, %{"body" => json})
-  end
-
-  def broadcast({:new_block_added_to_chain}) do
-  	Aehttpserver.Web.Endpoint.broadcast!("room:notifications", "new_block_added_to_chain", %{"body" => "!!!!!!New block added to chain!!!!!!"})
+  def broadcast_new_block_added_to_chain(block) do
+  	Aehttpserver.Web.Endpoint.broadcast!("room:notifications", "new_block_added_to_chain", %{"body" => Serialization.block(block, :serialize)})
   end
 end
