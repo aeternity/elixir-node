a3096c4a2572e984d43fb4c20ee6fa100c18faca
Nedelcho Delchev <nedelcho_delchev@abv.bg>
2018-01-18 17:31:02 +0200

    [GH-#178] Initial structure for the interpreter

--- apps/aernold/lib/aernold.ex
+++ apps/aernold/lib/aernold.ex
@@ -1,7 +1,47 @@
 defmodule Aernold do
 
+  defp reduce_to_value({:int, _line, value}, _state) do
+
+  end
+
+  defp reduce_to_value({:string, _line, string}, state) do
+
+  end
+
+  defp reduce_to_value({:char, _line, char}, state) do
+
+  end
+
+  defp reduce_to_value({:hex, _line, hex}, state) do
+
+  end
+
+  defp reduce_to_value({:add_op, lhs, rhs}, state) do
+    #reduce_to_value(lhs, state) + reduce_to_value(rhs, state)
+  end
+
+  defp reduce_to_value({:sub_op, lhs, rhs}, state) do
+    #reduce_to_value(lhs, state) - reduce_to_value(rhs, state)
+  end
+
+  defp reduce_to_value({:mul_op, lhs, rhs}, state) do
+    #reduce_to_value(lhs, state) * reduce_to_value(rhs, state)
+  end
+
+  defp reduce_to_value({:div_op, lhs, rhs}, state) do
+    #reduce_to_value(lhs, state) / reduce_to_value(rhs, state)
+  end
+
+  def eval([], state) do
+    #state
+  end
+
+  def process_ast(ast) do
+    #eval(tree, %{})
+  end
+
   def parse_string(string) do
-    parse(string)
+    #parse(string)
   end
 
   def parse_file(filename) do
