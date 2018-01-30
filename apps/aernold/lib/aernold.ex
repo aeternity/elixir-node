37a369dbdfb5eb4ff3c8af04c195103ca46e8db6
Nedelcho Delchev <nedelcho_delchev@abv.bg>
2018-01-30 17:55:11 +0200

    [GH-#178] If statement works partially, bug fixes and removed warnings

--- apps/aernold/lib/aernold.ex
+++ apps/aernold/lib/aernold.ex
@@ -4,27 +4,27 @@ defmodule Aernold do
     int
   end
 
-  defp reduce_to_value({:bool, bool}, state) do
+  defp reduce_to_value({:bool, bool}, _state) do
     bool
   end
 
-  defp reduce_to_value({:hex, hex}, state) do
+  defp reduce_to_value({:hex, hex}, _state) do
     to_string(hex)
   end
 
-  defp reduce_to_value({:char, char}, state) do
+  defp reduce_to_value({:char, char}, _state) do
     [char]
   end
 
-  defp reduce_to_value({:string, string}, state) do
+  defp reduce_to_value({:string, string}, _state) do
     string
   end
 
-  defp reduce_to_value({:id, id}, state) do
+  defp reduce_to_value({:id, id}, _state) do
     id
   end
 
-  defp reduce_to_value({:type, type}, state) do
+  defp reduce_to_value({:type, type}, _state) do
     type
   end
 
@@ -48,46 +48,21 @@ defmodule Aernold do
         if Map.has_key?(state, lhs) && Map.has_key?(state, rhs) do
           lhs_value = Map.get(state, lhs)
           rhs_value = Map.get(state, rhs)
-          if lhs_value == rhs_value, do: :true, else: :false
+          if lhs_value == rhs_value, do: true, else: false
         end
     end
   end
 
   defp evaluate_ast([{:contract, _}, rhs | tail], state) do
-    IO.puts "1-----------------RHS----------------1"
-    IO.inspect(rhs)
-    IO.puts "1..................TAIL...................1"
-    IO.inspect(tail)
     rhs_value = reduce_to_value(rhs, state)
-    IO.puts "1----------------reduced RHS --------------1"
-    IO.inspect(rhs_value)
     evaluate_ast(tail, Map.merge(state, %{:contract => rhs_value}))
   end
 
-  defp evaluate_ast([{lhs, op, rhs} | tail], state) do
-    IO.puts "2-----------------LHS------------------2"
-    IO.inspect(lhs)
-    IO.puts "2-----------------RHS----------------2"
-    IO.inspect(rhs)
-    IO.puts "2..................TAIL...................2"
-    IO.inspect(tail)
-    lhs_value = reduce_to_value(lhs, state)
-    IO.puts "2-----------------reduced LHS side-------------2"
-    IO.inspect(lhs_value)
-    rhs_value = reduce_to_value(rhs, state)
-    IO.puts "2-----------------reduced RHS side-------------2"
-    IO.inspect(rhs_value)
-    evaluate_ast(tail, Map.merge(state, %{lhs_value => rhs_value}))
-  end
-
-  defp evaluate_ast([{:def_var, id, type, value} | tail], state) do
+  defp evaluate_ast([{{:def_var, id, type, value}} | tail], state) do
     hex_regex = ~r{0[xX][0-9a-fA-F]+}
     extracted_id = reduce_to_value(id, state)
-    IO.inspect("Extracted id = #{extracted_id}")
     extracted_type = reduce_to_value(type, state)
-    IO.inspect("Extracted type = #{extracted_type}")
     extracted_value = reduce_to_value(value, state)
-    IO.inspect("Extracted value = #{extracted_value}")
     cond do
       extracted_type == 'Int' && !(is_integer(extracted_value)) ->
         throw({:error, "The value of (#{extracted_id}) must be Integer"})
@@ -103,17 +78,27 @@ defmodule Aernold do
         evaluate_ast(tail, Map.merge(state, %{extracted_id => {extracted_type, extracted_value}}))
     end
   end
-  #it doesnt work correctly right now
+
+  defp evaluate_ast([{:decl_var, id, type} | tail], state) do
+    extracted_id = reduce_to_value(id, state)
+    extracted_type = reduce_to_value(type, state)
+    evaluate_ast(tail, Map.merge(state, %{extracted_id => extracted_type}))
+  end
+
   defp evaluate_ast([{:if_statement, condition, body} | tail], state) do
     extracted_condition = reduce_to_value(condition, state)
-    IO.inspect(extracted_condition)
-    extracted_body = reduce_to_value(body, state)
-    IO.inspect(extracted_body)
     if extracted_condition do
-      evaluate_ast(tail, Map.merge(state, %{"if"=> "works"}))
+      evaluate_ast([body], state)
+    else
+      evaluate_ast(tail, Map.merge(state, %{"if" => extracted_condition}))
     end
   end
 
+  defp evaluate_ast([{lhs, op, rhs} | tail], state) do
+    lhs_value = reduce_to_value(lhs, state)
+    rhs_value = reduce_to_value(rhs, state)
+    evaluate_ast(tail, Map.merge(state, %{lhs_value => rhs_value}))
+  end
 
   defp evaluate_ast([], state) do
     state
