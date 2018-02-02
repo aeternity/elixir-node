64821187843bfb9edf68091c8789150a59563dbc
HristianHristov <hristian.hristov@quanterall.com>
2018-02-02 16:27:45 +0200

    [GH-#197] Created structure for signing a contract

---
+++ apps/aecore/lib/aecore/structures/contract_sign.ex
@@ -0,0 +1,25 @@
+defmodule Aecore.Structures.ContractSignTx do
+  require Logger
+
+  @type t :: %ContractSignTx{
+    signature: binary(),
+    pub_key: binary(),
+    contract_hash: binary(),
+    fee: integer()
+  }
+
+  defstruct [:signature,
+             :pub_key,
+             :contract_hash,
+             :fee
+            ]
+  use ExConstructor
+
+  def create(signature, pub_key, contract_hash, fee) do
+    {:ok, %ContractSignTx{signature: signature,
+                          pub_key: pub_key,
+                          contract_hash: contract_hash,
+                          fee: fee}}
+  end
+
+end
