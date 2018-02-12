aa8148d516b573c2071dea843eeab44979316c06
gspasov <georgi.spasov@quanterall.com>
2018-02-12 17:38:50 +0200

    [GH-#132] Fixed the GET /info, 'Invalid password' error

--- apps/aehttpserver/lib/aehttpserver/web/controllers/info_controller.ex
+++ apps/aehttpserver/lib/aehttpserver/web/controllers/info_controller.ex
@@ -7,8 +7,6 @@ defmodule Aehttpserver.Web.InfoController do
   alias Aecore.Wallet.Worker, as: Wallet
   alias Aecore.Peers.Worker, as: Peers
 
-  @wallet_pass "1234"
-
   require Logger
 
   def info(conn, _params) do
@@ -24,12 +22,13 @@ defmodule Aehttpserver.Web.InfoController do
 
     own_nonce = Peers.get_peer_nonce()
 
-    pubkey = Wallet.get_public_key(@wallet_pass)
+    pubkey = Wallet.get_public_key(Wallet.get_aewallet_pass())
     pubkey_hex = Base.encode16(pubkey)
 
     #Add whoever's getting our info
     peer_port_headers = Plug.Conn.get_req_header(conn, "peer_port")
     peer_nonce_headers = Plug.Conn.get_req_header(conn, "nonce")
+
     if !Enum.empty?(peer_port_headers) && !Enum.empty?(peer_nonce_headers) do
       peer_ip = conn.peer |> elem(0) |> Tuple.to_list |> Enum.join(".")
       peer_port = peer_port_headers   |> Enum.at(0) |> to_string()
@@ -41,8 +40,6 @@ defmodule Aehttpserver.Web.InfoController do
         Peers.schedule_add_peer(peer, peer_nonce)
       end
     end
-
-
 
     json(conn, %{current_block_version: top_block.header.version,
                  current_block_height: top_block.header.height,
