1f69a0eea0eb72748ed82ebde2092b3a277c7448
Artur Puzio <cytadela8@interia.pl>
2018-03-08 22:54:39 +0100

    [GH-247] Added .iex.exs file with aliases

---
+++ .iex.exs
@@ -0,0 +1,12 @@
+alias Aecore.Structures.Header
+alias Aecore.Structures.Block
+alias Aecore.Structures.SpendTx
+alias Aecore.Structures.SignedTx
+alias Aecore.Structures.Account
+alias Aecore.Chain.Worker, as: Chain
+alias Aecore.Chain.ChainState, as: ChainState
+alias Aecore.Wallet.Worker, as: Wallet
+alias Aecore.Miner.Worker, as: Miner
+alias Aecore.Txs.Pool.Worker, as: Pool
+alias Aecore.Peers.Worker, as: Peers
+alias Aecore.Peers.Sync
