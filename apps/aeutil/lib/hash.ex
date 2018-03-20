797f6bfa907172899e811a29ea0770f8f2eb4a07
Philipp Piwo <github@piwo.space>
2018-03-20 11:44:55 +0100

    [GH-#269] generalized hashing  - added sha3 and blake2b library

---
+++ apps/aeutil/lib/hash.ex
@@ -0,0 +1,22 @@
+defmodule Aeutil.Hash do
+  @hash_bytes_size 32
+
+  @spec hash_blake2b(binary()) :: binary()
+  def hash_blake2b(data) when is_binary(data) do
+    {:ok, hash} = :enacl.generichash(@hash_bytes_size, data)
+    hash
+  end
+
+  @spec hash_sha3(binary()) :: binary()
+  def hash_sha3(data) when is_binary(data) do
+    :sha3.hash(@hash_bytes_size * 8, data)
+  end
+
+  @spec hash_sha256(binary()) :: binary()
+  def hash_sha256(data) when is_binary(data) do
+    :crypto.hash(:sha256, data)
+  end
+
+  @spec hash(binary()) :: binary()
+  defdelegate hash(opts), to: __MODULE__, as: :hash_blake2b
+end
