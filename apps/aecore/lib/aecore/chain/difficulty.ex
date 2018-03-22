0369fb245d4c48bc013eb369f033d3803de01b4f
HristianHristov <hristian.hristov@quanterall.com>
2018-03-23 00:25:24 +0200

    [GH-#258] Added target to difficulty calculation, refactored target recalculation to be in scientific

--- apps/aecore/lib/aecore/chain/difficulty.ex
+++ apps/aecore/lib/aecore/chain/difficulty.ex
@@ -1,6 +1,6 @@
 defmodule Aecore.Chain.Difficulty do
   alias Aecore.Structures.Block
-
+  alias Aeutil.Scientific
   @number_of_blocks 10
   @max_difficulty_change Application.get_env(:aecore, :pow)[:max_difficulty_change]
   @target_distance 30_000
@@ -17,9 +17,10 @@ defmodule Aecore.Chain.Difficulty do
       latest_block.header.difficulty_target
     else
       distance = calculate_distance(list)
+      int_target = Scientific.scientific_to_integer(latest_block.header.difficulty_target)
 
       next_difficulty =
-        (latest_block.header.difficulty_target * (@target_distance / distance))
+        (int_target * (@target_distance / distance))
         |> Float.ceil()
         |> round()
 
@@ -29,15 +30,17 @@ defmodule Aecore.Chain.Difficulty do
 
   @spec limit_max_difficulty_change(integer(), integer()) :: integer()
   def limit_max_difficulty_change(calculated_next_difficulty, last_difficult) do
-    cond do
-      calculated_next_difficulty - last_difficult > @max_difficulty_change ->
-        last_difficult + @max_difficulty_change
+    int_last_difficult = Scientific.scientific_to_integer(last_difficult)
 
-      last_difficult - calculated_next_difficulty > @max_difficulty_change ->
-        last_difficult - @max_difficulty_change
+    cond do
+      calculated_next_difficulty - int_last_difficult > @max_difficulty_change ->
+        Scientific.integer_to_scientific(int_last_difficult + @max_difficulty_change)
+
+      int_last_difficult - calculated_next_difficulty > @max_difficulty_change ->
+        Scientific.integer_to_scientific(int_last_difficult - @max_difficulty_change)
 
       true ->
-        calculated_next_difficulty
+        Scientific.integer_to_scientific(calculated_next_difficulty)
     end
   end
 
--- apps/aeutil/lib/scientific.ex
+++ apps/aeutil/lib/scientific.ex
@@ -1,48 +1,64 @@
 defmodule Aeutil.Scientific do
-
   use Bitwise
 
-  @highest_target_scientific 0x2100ffff
-  @highest_target_integer 0xffff000000000000000000000000000000000000000000000000000000000000
+  @highest_target_scientific 0x2100FFFF
+  @highest_target_integer 0xFFFF000000000000000000000000000000000000000000000000000000000000
   @nonce_bits 64
-  @max_nonce 0xffffffffffffffff
-  @nonce_range 1000000000000000000000000
+  @max_nonce 0xFFFFFFFFFFFFFFFF
+  @nonce_range 1_000_000_000_000_000_000_000_000
 
   def scientific_to_integer(scientific) do
     {exp, significand} = break_scientific(scientific)
     exp3 = exp - 3
+
     case exp >= 0 do
-      true -> bsl(significand, (8 * exp3))
-      false -> bsr(significand, (-8 * exp3))
+      true -> bsl(significand, 8 * exp3)
+      false -> bsr(significand, -8 * exp3)
     end
   end
 
   def integer_to_scientific(integer) do
     {exp, significand} = int_to_sci(integer, 3)
+
     case exp >= 0 do
       true -> bsl(exp, 24) + significand
       false -> bsl(-exp, 24) + 0x800000 + significand
     end
   end
 
-
-
-  defp int_to_sci(integer, exp) when integer > 0x7fffff do
-     int_to_sci(bsr(integer, 8), exp + 1)
+  def target_to_difficulty(target) do
+    @highest_target_integer / scientific_to_integer(target)
   end
+
+  def pick_nonce() do
+    band(:rand.uniform(@nonce_range), @max_nonce)
+  end
+
+  def next_nonce(nonce) do
+    band(nonce + 1, @max_nonce)
+  end
+
+  defp int_to_sci(integer, exp) when integer > 0x7FFFFF do
+    int_to_sci(bsr(integer, 8), exp + 1)
+  end
+
   defp int_to_sci(integer, exp) when integer < 0x008000 do
     int_to_sci(bsl(integer, 8), exp - 1)
   end
+
   defp int_to_sci(integer, exp) do
     {exp, integer}
   end
 
   defp break_scientific(scientific) do
-    significand_mask = bsl(1, 24) -1
+    significand_mask = bsl(1, 24) - 1
+
     exp =
       bxor(scientific, significand_mask)
       |> bsr(24)
+
     significand = band(scientific, significand_mask)
+
     case band(0x800000, significand) do
       0 -> {exp, significand}
       _ -> {exp, significand - 0x800000}
