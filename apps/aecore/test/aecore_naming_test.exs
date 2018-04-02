ee904f49c8e7b8635f0f307357070f3e5c4c748d
Philipp Piwo <github@piwo.space>
2018-04-02 14:45:53 +0200

    [GH-#274] add revoke name test

--- apps/aecore/test/aecore_naming_test.exs
+++ apps/aecore/test/aecore_naming_test.exs
@@ -65,8 +65,9 @@ defmodule AecoreNamingTest do
     assert first_name.status == :claimed
     assert first_name.pointers == ["{\"test\": 2}"]
 
-    target_pub_key = Wallet.get_public_key("M/0/1")
-    {:ok, transfer} = Account.name_transfer("test.aet", target_pub_key, 5)
+    transfer_to_priv = Aecore.Wallet.Worker.get_private_key("m/0/1")
+    transfer_to_pub = Aecore.Wallet.Worker.to_public_key(transfer_to_priv)
+    {:ok, transfer} = Account.name_transfer("test.aet", transfer_to_pub, 5)
     Pool.add_transaction(transfer)
     Miner.mine_sync_block_to_chain()
 
@@ -75,8 +76,27 @@ defmodule AecoreNamingTest do
     [first_name] = naming_state
     assert first_name.hash == NameUtil.normalized_namehash!("test.aet")
     assert first_name.name == "test.aet"
-    assert first_name.owner == Wallet.get_public_key("M/0/1")
+    assert first_name.owner == transfer_to_pub
     assert first_name.status == :claimed
+    assert first_name.pointers == ["{\"test\": 2}"]
+
+    #fund transfered account
+    {:ok, spend} = Account.spend(transfer_to_pub, 5, 5)
+    Pool.add_transaction(spend)
+    Miner.mine_sync_block_to_chain()
+
+    next_nonce =  Map.get(Chain.chain_state().accounts, transfer_to_pub, %{nonce: 0}).nonce + 1
+    {:ok, revoke} = Account.name_revoke(transfer_to_pub, transfer_to_priv, "test.aet", 5, next_nonce)
+    Pool.add_transaction(revoke)
+    Miner.mine_sync_block_to_chain()
+
+    naming_state = Map.values(Chain.chain_state().naming)
+    assert 1 == Enum.count(naming_state)
+    [first_name] = naming_state
+    assert first_name.hash == NameUtil.normalized_namehash!("test.aet")
+    assert first_name.name == "test.aet"
+    assert first_name.owner == transfer_to_pub
+    assert first_name.status == :revoked
     assert first_name.pointers == ["{\"test\": 2}"]
   end
 end
