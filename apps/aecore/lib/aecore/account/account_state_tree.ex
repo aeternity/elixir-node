680afe4042ca8ba9112549d231466ac0776971b7
danielaivanova <danielaivanova_@abv.bg>
2018-04-23 18:35:08 +0300

    [GH-#341] Synchronizing API account state tree to work with API patricia merkle tree.

--- apps/aecore/lib/aecore/account/account_state_tree.ex
+++ apps/aecore/lib/aecore/account/account_state_tree.ex
@@ -5,56 +5,61 @@ defmodule Aecore.Account.AccountStateTree do
   alias Aecore.Account.Account
   alias Aecore.Wallet.Worker, as: Wallet
   alias Aeutil.Serialization
-  @type encoded_account_state :: binary()
+  alias Aeutil.PatriciaMerkleTree
 
-  # abstract datatype representing a merkle tree
-  @type tree :: tuple()
-  @type accounts_state :: tree()
+  require Logger
+
+  @type accounts_state :: Trie.t()
   @type hash :: binary()
 
-  @spec init_empty() :: tree()
+  @spec init_empty() :: Trie.t()
   def init_empty do
-    :gb_merkle_trees.empty()
+    PatriciaMerkleTree.new(:account)
   end
 
-  @spec put(tree(), Wallet.pubkey(), Account.t()) :: tree()
-  def put(tree, key, value) do
-    serialized_account_state = Serialization.account_state(value, :serialize)
-    :gb_merkle_trees.enter(key, serialized_account_state, tree)
+  @spec put(Trie.t(), Wallet.pubkey(), Account.t()) :: Trie.t()
+  def put(trie, key, value) do
+    serialized = Serialization.serialize_term(value)
+    PatriciaMerkleTree.enter(trie, key, serialized)
   end
 
-  @spec get(tree(), Wallet.pubkey()) :: Account.t()
-  def get(tree, key) do
-    account_state = :gb_merkle_trees.lookup(key, tree)
-    Serialization.account_state(account_state, :deserialize)
+  @spec get(Trie.t(), Wallet.pubkey()) :: Account.t()
+  def get(trie, key) do
+    trie
+    |> PatriciaMerkleTree.lookup(key)
+    |> Serialization.deserialize_term()
   end
 
-  def has_key?(tree, key) do
-    :gb_merkle_trees.lookup(key, tree) != :none
+  def has_key?(trie, key) do
+    PatriciaMerkleTree.lookup(trie, key) != :none
   end
 
-  @spec delete(tree(), Wallet.pubkey()) :: tree()
-  def delete(tree, key) do
-    :gb_merkle_trees.delete(key, tree)
+  # @spec delete(tree(), Wallet.pubkey()) :: tree()
+  # def delete(tree, key) do
+  #   :gb_merkle_trees.delete(key, tree)
+  # end
+
+  # @spec balance(tree()) :: tree()
+  # def balance(tree) do
+  #   :gb_merkle_trees.balance(tree)
+  # end
+
+  @spec root_hash(Trie.t()) :: hash()
+  def root_hash(trie) do
+    PatriciaMerkleTree.root_hash(trie)
   end
 
-  @spec balance(tree()) :: tree()
-  def balance(tree) do
-    :gb_merkle_trees.balance(tree)
-  end
-
-  @spec root_hash(tree()) :: hash()
-  def root_hash(tree) do
-    :gb_merkle_trees.root_hash(tree)
-  end
-
-  @spec reduce(tree(), any(), fun()) :: any()
+  @spec reduce(Trie.t(), any(), fun()) :: any()
   def reduce(tree, acc, fun) do
-    :gb_merkle_trees.foldr(fun, acc, tree)
+    Logger.error(fn ->
+      "#{__MODULE__}: Calculate total tokens. Error function not implemented (reduce)"
+    end)
+
+    0
   end
 
-  @spec size(tree()) :: non_neg_integer()
+  @spec size(Trie.t()) :: non_neg_integer()
   def size(tree) do
-    :gb_merkle_trees.size(tree)
+    1
   end
 end
