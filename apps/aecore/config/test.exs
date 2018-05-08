cbf4c93939e056944c9546d0b00bc071f3123c5e
meivantodorov <meivantodorov@gmail.com>
2018-05-08 19:08:11 +0300

    [GH-#244] Fixed missing directories and wrong miner on test env

--- apps/aecore/config/test.exs
+++ apps/aecore/config/test.exs
@@ -39,7 +39,7 @@ config :aecore, :persistence, path: persistence_path |> Path.absname() |> Path.j
 config :aecore, :pow,
   new_candidate_nonce_count: 10,
   bin_dir: Path.absname("apps/aecore/priv/cuckoo/bin"),
-  params: {"./lean", "-t 5", 16},
+  params: {"./lean16", "-t 5", 16},
   max_target_change: 0,
   genesis_header: %{
     height: 0,
