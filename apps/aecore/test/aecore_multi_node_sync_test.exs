2cd8ccb1ee347f1f273029856d34d9c7fa89e635
Oleg Ivanov <oleg.ivanov@quanterall.com>
2018-05-17 15:38:54 +0300

    [GH-#307] Added sync test with the second node running in separate folder

--- apps/aecore/test/aecore_multi_node_sync_test.exs
+++ apps/aecore/test/aecore_multi_node_sync_test.exs
@@ -1,15 +1,61 @@
 defmodule MultiNodeSyncTest do
   use ExUnit.Case
+
+  alias Aecore.Peers.Worker, as: Peers
+  alias Aecore.Miner.Worker, as: Miner
+  alias Aecore.Chain.Worker, as: Chain
   @tag timeout: 180000
 
+  setup do
+    Peers.start_link([])
+    Miner.start_link([])
+    Chain.start_link([])
+    []
+  end
+
   test "run second node" do
+
+    str = "defmodule MultiNodeSyncTest do
+      use ExUnit.Case
+
+      alias Aecore.Peers.Worker, as: Peers
+      alias Aecore.Miner.Worker, as: Miner
+      alias Aecore.Chain.Worker, as: Chain
+      @tag timeout: 180000
+
+      setup do
+        Peers.start_link([])
+        Miner.start_link([])
+        Chain.start_link([])
+        []
+      end
+
+      test \"run_sync\" do
+        Peers.add_peer(\"localhost:4000\")
+        :timer.sleep(5000)
+        Miner.mine_sync_block_to_chain()
+        :timer.sleep(5000)
+        IO.puts \"Second node:\"
+        IO.inspect Chain.top_block()
+      end
+    end"
     System.cmd("cp", ["-R", "../../../elixir-node", "../../../elixir-node2"])
     System.cmd "rm", ["-rf", "../../../elixir-node2/apps/aecore/test/"]
+    System.cmd "mkdir", ["../../../elixir-node2/apps/aecore/test/"]
+    System.cmd("cp", ["../../../elixir-node/apps/aecore/test/test_helper.exs", "../../../elixir-node2/apps/aecore/test/test_helper.exs"])
+    System.cmd("cp", ["../../../elixir-node/apps/aecore/test/test_utils.ex", "../../../elixir-node2/apps/aecore/test/test_utils.ex"])
+    {:ok, file} = File.open "../../../elixir-node2/apps/aecore/test/multi_node_sync_test.exs", [:write]
+    IO.write(file, str)
     System.cmd "rm", ["../../../elixir-node2/apps/aehttpclient/test/aehttpclient_test.exs"]
-    System.cmd "sed", ["-i", "s/4000/4001/", "../../../elixir-node2/apps/aehttpserver/config/dev.exs"]
-    System.cmd "sed", ["-i", "s/4000/4001/", "../../../elixir-node2/apps/aehttpserver/config/test.exs"]
+    System.cmd "sed", ["-i", "s/4000/4002/", "../../../elixir-node2/apps/aehttpserver/config/dev.exs"]
+    System.cmd "sed", ["-i", "s/4000/4002/", "../../../elixir-node2/apps/aehttpserver/config/test.exs"]
 
+    Miner.mine_sync_block_to_chain
     System.cmd "mix", ["test"], cd: "../../../elixir-node2", into: IO.stream(:stdio, :line)
+    IO.puts "First node:"
+    IO.inspect Chain.top_block()
     System.cmd "rm", ["-rf", "../../../elixir-node2"]
   end
+
+
 end
