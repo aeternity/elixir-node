a7e13adc6a11f6ed5a78fb8c28b823b7e3115a71
Oleg Ivanov <oleg.ivanov@quanterall.com>
2018-05-18 12:29:10 +0300

    [GH-#307] formatted code

--- apps/aecore/test/aecore_multi_node_sync_test.exs
+++ apps/aecore/test/aecore_multi_node_sync_test.exs
@@ -5,7 +5,7 @@ defmodule MultiNodeSyncTest do
   alias Aecore.Peers.Worker, as: Peers
   alias Aecore.Miner.Worker, as: Miner
   alias Aecore.Chain.Worker, as: Chain
-  @tag timeout: 180000
+  @tag timeout: 180_000
 
   setup do
     Peers.start_link([])
@@ -15,7 +15,6 @@ defmodule MultiNodeSyncTest do
   end
 
   test "run second node" do
-
     str = "defmodule MultiNodeSyncTest do
       use ExUnit.Case
 
@@ -43,22 +42,43 @@ defmodule MultiNodeSyncTest do
       end
     end"
     System.cmd("cp", ["-R", "../../../elixir-node", "../../../elixir-node2"])
-    System.cmd "rm", ["-rf", "../../../elixir-node2/apps/aecore/test/"]
-    System.cmd "mkdir", ["../../../elixir-node2/apps/aecore/test/"]
-    System.cmd("cp", ["../../../elixir-node/apps/aecore/test/test_helper.exs", "../../../elixir-node2/apps/aecore/test/test_helper.exs"])
-    System.cmd("cp", ["../../../elixir-node/apps/aecore/test/test_utils.ex", "../../../elixir-node2/apps/aecore/test/test_utils.ex"])
-    {:ok, file} = File.open "../../../elixir-node2/apps/aecore/test/multi_node_sync_test.exs", [:write]
-    IO.write(file, str)
-    System.cmd "rm", ["../../../elixir-node2/apps/aehttpclient/test/aehttpclient_test.exs"]
-    System.cmd "sed", ["-i", "s/4000/4002/", "../../../elixir-node2/apps/aehttpserver/config/dev.exs"]
-    System.cmd "sed", ["-i", "s/4000/4002/", "../../../elixir-node2/apps/aehttpserver/config/test.exs"]
+    System.cmd("rm", ["-rf", "../../../elixir-node2/apps/aecore/test/"])
+    System.cmd("mkdir", ["../../../elixir-node2/apps/aecore/test/"])
 
-    Miner.mine_sync_block_to_chain
-    System.cmd "mix", ["test"], cd: "../../../elixir-node2", into: IO.stream(:stdio, :line)
-    {:ok, top_block_binary} = File.read "../../../elixir-node2/apps/aecore/block.txt"
+    System.cmd("cp", [
+      "../../../elixir-node/apps/aecore/test/test_helper.exs",
+      "../../../elixir-node2/apps/aecore/test/test_helper.exs"
+    ])
+
+    System.cmd("cp", [
+      "../../../elixir-node/apps/aecore/test/test_utils.ex",
+      "../../../elixir-node2/apps/aecore/test/test_utils.ex"
+    ])
+
+    {:ok, file} =
+      File.open("../../../elixir-node2/apps/aecore/test/multi_node_sync_test.exs", [:write])
+
+    IO.write(file, str)
+    System.cmd("rm", ["../../../elixir-node2/apps/aehttpclient/test/aehttpclient_test.exs"])
+
+    System.cmd("sed", [
+      "-i",
+      "s/4000/4002/",
+      "../../../elixir-node2/apps/aehttpserver/config/dev.exs"
+    ])
+
+    System.cmd("sed", [
+      "-i",
+      "s/4000/4002/",
+      "../../../elixir-node2/apps/aehttpserver/config/test.exs"
+    ])
+
+    Miner.mine_sync_block_to_chain()
+    System.cmd("mix", ["test"], cd: "../../../elixir-node2", into: IO.stream(:stdio, :line))
+    {:ok, top_block_binary} = File.read("../../../elixir-node2/apps/aecore/block.txt")
     top_block = :erlang.binary_to_term(top_block_binary)
 
     assert Chain.top_block() == top_block
-    System.cmd "rm", ["-rf", "../../../elixir-node2"]
+    System.cmd("rm", ["-rf", "../../../elixir-node2"])
   end
 end
