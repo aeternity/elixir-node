a84f5e183552ef06c609752f859dcd77eb400a79
Oleg Ivanov <oleg.ivanov@quanterall.com>
2018-06-13 10:00:30 +0300

    [GH-#417] added functionality to see alive ports, send command to node

--- apps/aeutil/lib/multi_node_test_framework.ex
+++ apps/aeutil/lib/multi_node_test_framework.ex
@@ -14,8 +14,16 @@ defmodule Aeutil.MultiNodeTestFramework do
     GenServer.call(__MODULE__, {:new_node, port})
   end
 
-  def alive_processes() do
-    GenServer.call(__MODULE__, {:alive_processes})
+  def alive_ports() do
+    GenServer.call(__MODULE__, {:alive_ports})
+  end
+
+  def get_node_chainstate(node_name) do
+    GenServer.call(__MODULE__, {:get_node_chainstate, node_name})
+  end
+
+  def command(node_name, command) do
+    GenServer.call(__MODULE__, {:command, node_name, command})
   end
 
   def run_nodes_test() do
@@ -26,12 +34,23 @@ defmodule Aeutil.MultiNodeTestFramework do
     {:reply, state, state}
   end
 
-  def handle_call({:alive_processes}, _, state) do
-    alive_processes = for {name, pid} <- state, into: %{}, do: {name, Process.alive?(pid)}
-    {:reply, alive_processes, state}
+  def handle_call({:get_node_chainstate, node_name}, _, state) do
+    result = Port.command(state[node_name].port, "Aecore.Chain.Worker.top_block()")
+    {:reply, result, state}
+  end
+
+  def handle_call({:alive_ports}, _, state) do
+    alive_ports = for {name, info} <- state, do: Port.info(state[name].port)
+    {:reply, alive_ports, state}
+  end
+
+  def handle_call({:command, node_name, command}, _, state) do
+    result = Port.command(state[node_name].port, command)
+    {:reply, result, state}
   end
 
   def handle_info({port, {:data, result}}, state) do
+    IO.puts "Elixir got: #{inspect result}"
     {:noreply, state}
   end
 
