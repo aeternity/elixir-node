ff94e9ae64c4a5e16293657b8e24e51d14c9286c
danielaivanova <danielaivanova_@abv.bg>
2018-07-10 17:38:56 +0300

    [GH-#468] Fixed function delete_expired

--- apps/aecore/lib/aecore/oracle/tx/oracle_response_tx.ex
+++ apps/aecore/lib/aecore/oracle/tx/oracle_response_tx.ex
@@ -13,6 +13,7 @@ defmodule Aecore.Oracle.Tx.OracleResponseTx do
   alias Aecore.Account.Account
   alias Aecore.Account.AccountStateTree
   alias Aecore.Chain.Chainstate
+  alias Aeutil.Serialization
 
   @type payload :: %{
           query_id: binary(),
@@ -95,7 +96,7 @@ defmodule Aecore.Oracle.Tx.OracleResponseTx do
         has_response: true
     }
 
-    updated_oracle_state = OracleStateTree.insert_query(oracles, updated_interaction_objects)
+    updated_oracle_state = OracleStateTree.enter_query(oracles, updated_interaction_objects)
 
     {:ok, {updated_accounts_state, updated_oracle_state}}
   end
