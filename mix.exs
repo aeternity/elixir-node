a2e5312a2c02c6ee3d4530e479321f1f19199ec5
Artur64 <artur.kratt@quanterall.com>
2018-07-24 10:35:16 +0300

    [GH-#432] Modified formatter.exs file

--- mix.exs
+++ mix.exs
@@ -71,7 +71,6 @@ defmodule EpochElixir.Mixfile do
       {:gproc, "~> 0.6.1"},
       {:ex_rlp, "~> 0.2.1"},
       {:merkle_patricia_tree, git: "https://github.com/aeternity/elixir-merkle-patricia-tree.git"}
-
     ]
   end
 end
