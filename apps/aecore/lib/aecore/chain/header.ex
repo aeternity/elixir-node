3f0c937250292ed6d25a34ceb409806166f18204
Artur Puzio <cytadela8@interia.pl>
2018-08-08 02:33:03 +0200

    [GH-352] Removed magic numbers

--- apps/aecore/lib/aecore/chain/header.ex
+++ apps/aecore/lib/aecore/chain/header.ex
@@ -7,11 +7,21 @@ defmodule Aecore.Chain.Header do
   alias Aeutil.Bits
   alias Aecore.Keys.Wallet
 
+  @header_version_size 64
+  @header_height_size 64
   @txs_hash_size 32
   @header_hash_size 32
   @root_hash_size 32
+  @header_target_size 64
   @pow_size 168
+  @header_nonce_size 64
+  @header_time_size 64
+  # Should be adjusted to 32 after key algorithm change
   @pubkey_size 33
+
+  @pow_element_size 4
+  @pow_element_size_bits @pow_element_size * 8
+  @pow_length 42
 
   @type t :: %Header{
           height: non_neg_integer(),
@@ -81,16 +91,15 @@ defmodule Aecore.Chain.Header do
   @spec encode_to_binary(t()) :: binary()
   def encode_to_binary(%Header{} = header) do
     <<
-      header.version::64,
-      header.height::64,
+      header.version::@header_version_size,
+      header.height::@header_height_size,
       header.prev_hash::binary-size(@header_hash_size),
       header.txs_hash::binary-size(@txs_hash_size),
       header.root_hash::binary-size(@root_hash_size),
-      header.target::64,
+      header.target::@header_target_size,
       pow_to_binary(header.pow_evidence)::binary-size(@pow_size),
-      header.nonce::64,
-      header.time::64,
-      # pubkey should be adjusted to 32 bytes.
+      header.nonce::@header_nonce_size,
+      header.time::@header_time_size,
       header.miner::binary-size(@pubkey_size)
     >>
   end
@@ -101,16 +110,15 @@ defmodule Aecore.Chain.Header do
 
   @spec decode_from_binary(binary()) :: {:ok, Header.t()} | {:error, String.t()}
   def decode_from_binary(<<
-        version::64,
-        height::64,
+        version::@header_version_size,
+        height::@header_height_size,
         prev_hash::binary-size(@header_hash_size),
         txs_hash::binary-size(@txs_hash_size),
         root_hash::binary-size(@root_hash_size),
-        target::64,
+        target::@header_target_size,
         pow_evidence_bin::binary-size(@pow_size),
-        nonce::64,
-        # pubkey should be adjusted to 32 bytes.
-        time::64,
+        nonce::@header_nonce_size,
+        time::@header_time_size,
         miner::binary-size(@pubkey_size)
       >>) do
     with {:ok, pow_evidence} <- binary_to_pow(pow_evidence_bin) do
@@ -138,10 +146,10 @@ defmodule Aecore.Chain.Header do
 
   @spec pow_to_binary(list()) :: binary() | list()
   def pow_to_binary(pow) do
-    if is_list(pow) and Enum.count(pow) == 42 do
+    if is_list(pow) and Enum.count(pow) == @pow_length do
       list_of_pows =
         for evidence <- pow, into: <<>> do
-          <<evidence::32>>
+          <<evidence::@pow_element_size_bits>>
         end
 
       serialize_pow(list_of_pows, <<>>)
@@ -162,7 +170,7 @@ defmodule Aecore.Chain.Header do
 
   @spec serialize_pow(binary(), binary()) :: binary()
   defp serialize_pow(pow, acc) when pow != <<>> do
-    <<elem::binary-size(4), rest::binary>> = pow
+    <<elem::binary-size(@pow_element_size), rest::binary>> = pow
     serialize_pow(rest, acc <> elem)
   end
 
@@ -170,12 +178,12 @@ defmodule Aecore.Chain.Header do
     acc
   end
 
-  defp deserialize_pow(<<pow::32, rest::binary>>, acc) do
+  defp deserialize_pow(<<pow::@pow_element_size_bits, rest::binary>>, acc) do
     deserialize_pow(rest, List.insert_at(acc, -1, pow))
   end
 
   defp deserialize_pow(<<>>, acc) do
-    if Enum.count(Enum.filter(acc, fn x -> is_integer(x) and x >= 0 end)) == 42 do
+    if Enum.count(Enum.filter(acc, fn x -> is_integer(x) and x >= 0 end)) == @pow_length do
       {:ok, acc}
     else
       {:error, "#{__MODULE__} : Illegal PoW serialization"}
