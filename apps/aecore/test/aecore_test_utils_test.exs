024addfe11a4bafcffb2c13f1dd1268d08157a75
Artur Puzio <cytadela8@interia.pl>
2018-08-16 00:25:19 +0200

    [GH-534] Added TestUtils.clean_blockchain test

---
+++ apps/aecore/test/aecore_test_utils_test.exs
@@ -0,0 +1,36 @@
+defmodule AecoreTestUtilsTest do
+  @moduledoc """
+  Unit test for test_utils testing helper
+  """
+  use ExUnit.Case
+  alias Aecore.Keys
+  alias Aecore.Chain.Block
+  alias Aecore.Chain.Worker, as: Chain
+  alias Aecore.Account.Account
+
+  setup do
+    Code.require_file("test_utils.ex", "./test")
+  end
+
+  test "clean_blockchain" do
+    TestUtils.clean_blockchain()
+    check_is_clean()
+
+    TestUtils.assert_transactions_mined
+    TestUtils.assert_transactions_mined
+    TestUtils.assert_transactions_mined
+
+    {pubkey, _privkey} = Keys.keypair(:sign)
+    assert Account.balance(Chain.chain_state().accounts, pubkey) > 0
+
+    TestUtils.clean_blockchain()
+    check_is_clean()
+  end
+
+  def check_is_clean do
+    {pubkey, _privkey} = Keys.keypair(:sign)
+    assert Account.balance(Chain.chain_state().accounts, pubkey) == 0
+    assert Chain.top_height == 0
+    assert Chain.top_block == Block.genesis_block()
+  end
+end
