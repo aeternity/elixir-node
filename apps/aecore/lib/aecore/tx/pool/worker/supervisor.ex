c0117d10211b906f59b2bf65b798c05c0078d2f5
danielaivanova <danielaivanova_@abv.bg>
2018-09-12 12:44:32 +0300

    #584 Removed specs for start_link in supervisor modules

--- apps/aecore/lib/aecore/tx/pool/worker/supervisor.ex
+++ apps/aecore/lib/aecore/tx/pool/worker/supervisor.ex
@@ -5,18 +5,6 @@ defmodule Aecore.Tx.Pool.Worker.Supervisor do
 
   use Supervisor
 
-  @spec start_link([
-          {:debug, [:log | :statistics | :trace | {any(), any()}]}
-          | {:name, atom() | {:global, any()} | {:via, atom(), any()}}
-          | {:spawn_opt,
-             :link
-             | :monitor
-             | {:fullsweep_after, non_neg_integer()}
-             | {:min_bin_vheap_size, non_neg_integer()}
-             | {:min_heap_size, non_neg_integer()}
-             | {:priority, :high | :low | :normal}}
-          | {:timeout, :infinity | non_neg_integer()}
-        ]) :: :ignore | {:error, any()} | {:ok, pid()}
   def start_link(args) do
     Supervisor.start_link(__MODULE__, :ok, args)
   end
