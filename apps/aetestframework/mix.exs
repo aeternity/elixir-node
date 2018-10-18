64858570b0c1160001f5b79efc51182805c9254a
Philipp Piwo <github@piwo.space>
2018-10-18 21:53:26 +0200

    #687 always recompile project in CI, fix compile errors

--- apps/aetestframework/mix.exs
+++ apps/aetestframework/mix.exs
@@ -6,13 +6,20 @@ defmodule Aetestframework.MixProject do
       app: :aetestframework,
       version: "0.1.0",
       elixir: "~> 1.6",
-      start_permanent: Mix.env() == :prod
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
     ]
   end
 
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:aehttpclient, in_umbrella: true},
     ]
   end
 end
