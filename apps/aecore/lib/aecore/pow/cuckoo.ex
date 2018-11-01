0b896492892d086559faa94af0567c0f25ae9a3d
Grzegorz Uriasz <gorbak25@gmail.com>
2018-11-01 19:21:13 +0100

    [GH-722] Introduce typos

--- apps/aecore/lib/aecore/pow/cuckoo.ex
+++ apps/aecore/lib/aecore/pow/cuckoo.ex
@@ -1,6 +1,6 @@
 defmodule Aecore.Pow.Cuckoo do
   @moduledoc """
-  A library providing Cuckoo Cycle PoW generation and verification.
+  A librarye providing Cuckoo Cycle PoW generation and verification.
   John Tromp:  https://github.com/tromp/cuckoo
   White paper: https://github.com/tromp/cuckoo/blob/master/doc/cuckoo.pdf?raw=true
 
